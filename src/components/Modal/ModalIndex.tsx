ae6747dd3330982321d4656070c56a43d8a46686
Filippo Floramo <filifloramo@gmail.com>
2023-01-23 20:06:52 +0100

    fix import

--- src/components/Modal/ModalIndex.tsx
+++ src/components/Modal/ModalIndex.tsx
@@ -4,7 +4,7 @@ import ModalTextFields from './ModalTextFields/ModalTextFields';
 import ModalDatePickers from './ModalDatePickers/ModalDatePickers';
 import ModalCloseButton from './ModalCloseButton/ModalCloseButton';
 import ModalSearchButton from './ModalSearchButton/ModalSearchButton';
-import styles from './Modalindex.module.scss';
+import styles from './ModalIndex.module.scss';
 
 export default function ModalIndex(): JSX.Element {
   const { isCountrySearch } = useStateAtoms();
