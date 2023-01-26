b1081845426dea7cb396586e33965f90b1701ed6
Filippo Floramo <filifloramo@gmail.com>
2023-01-26 12:56:39 +0100

    add tooltip

--- src/components/Charts/ChartCounter/ChartCounter.tsx
+++ src/components/Charts/ChartCounter/ChartCounter.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { EmissionData } from "../../../interfaces/interfaces";
+import Tooltip from "../../Tooltip/Tooltip";
 import styles from './ChartCounter.module.scss';
 
 interface ChartCounterProps {
@@ -7,30 +8,37 @@ interface ChartCounterProps {
 }
 
 const getMean = (data: EmissionData[]) => {
-   let color;
+   let color: string;
+   let emoji: string;
 
    const mean = data.reduce((sum, currItem) => { return (sum + currItem.average); }, 0) / data.length;
    const dataMean = Number(mean.toFixed(2));
 
    if (dataMean < 2.5) {
       color = "#208759";
+      emoji = "✅";
    } else if (dataMean < 3.5) {
       color = "#ffc800";
+      emoji = "⚠️";
    } else {
       color = "red";
+      emoji = "☠️";
    }
 
-   return { dataMean, color };
+   return { dataMean, color, emoji };
 };
 
 
 export default function ChartCounter({ data }: ChartCounterProps): JSX.Element {
 
-   const { dataMean, color } = getMean(data);
+   const { dataMean, color, emoji } = getMean(data);
 
    return (
       <div className={styles.counter}>
-         Total average: <span style={{ color: color }}>{dataMean}</span>
+         <span>Total average: </span>
+         <Tooltip direction="top" content="Acceptable values are considered to be below 3.5">
+            <span style={{ color: color }}>{dataMean}</span> <span>{emoji}</span>
+         </Tooltip>
       </div >
    );
 }
