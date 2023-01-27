1c52d725c7df310b149bdb535ed43a376acdced2
Filippo Floramo <filifloramo@gmail.com>
2023-01-27 13:41:47 +0100

    add final animation and shadows

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -1,7 +1,25 @@
+import { motion, Variants } from 'framer-motion';
 import useStateAtoms from '../atoms/atoms';
 import ModalIndex from '../components/Modal/ModalIndex';
 import SearchTypeButton from '../components/SearchTypeButton/SearchTypeButton';
 import styles from '../styles/pages/Search.module.scss';
+
+const searchVariants: Variants = {
+  hidden: { opacity: 0, y: 20 },
+  animate: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      opacity: { duration: 0.1 },
+      y: {
+        duration: 0.2,
+        type: 'spring',
+        stiffness: 260,
+        damping: 20,
+      },
+    },
+  },
+};
 
 export default function Search(): JSX.Element {
   const { isModalOpen } = useStateAtoms();
@@ -10,14 +28,19 @@ export default function Search(): JSX.Element {
     <>
       {isModalOpen && <ModalIndex />}
       <div className={styles.search}>
-        <div className={styles.search_container}>
+        <motion.div
+          className={styles.search_container}
+          variants={searchVariants}
+          initial="hidden"
+          animate="animate"
+        >
           <h1 className={styles.search_cta}>Search by:</h1>
           <div className={styles.search_buttons}>
             <SearchTypeButton type="Country" />
             <p> or</p>
             <SearchTypeButton type="Coordinates" />
           </div>
-        </div>
+        </motion.div>
       </div>
     </>
   );
